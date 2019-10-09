abf360198d4ec18607812fb63fd5e2034a4e9534
lozye <lozye@qq.com>
2019-10-09 19:46:51 +0800

    v20191009 update time fn

--- timeignore.lua
+++ timeignore.lua
@@ -1,16 +1,19 @@
 -- 日期忽略
 timeignore = {}
 function timeignore:new(expire_s)
-    expire_s = expire_s or 30
-    _temp = {time = os.time(), hash = {}, expire = expire_s}
+    _temp = {}
     setmetatable(_temp, self)
     self.__index = self
+    --wow 获取世界方法
+    self.time = GetTime()    
+    self.hash = {}
+    self.expire = expire_s
     return _temp
 end
 
 -- 尝试添加，为false表示拦截
 function timeignore:add(text)
-    local tick = os.time()
+    local tick = GetTime()   
     if (tick - self.time > self.expire) then
         self.hash = {}
         self.time = tick
